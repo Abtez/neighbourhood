c48a9a6250a7af1517e44f7a241725051222470f
Abzed <ibraabzed@gmail.com>
2020-11-05 12:50:33 +0300

    add Name test

--- neighbour/tests.py
+++ neighbour/tests.py
@@ -8,11 +8,20 @@ class ProfileTestCase(TestCase):
     def setUp(self):
         hood_name = Name(neighbourhood_name='CBD')
         user = User.objects.create_user(username='testusereres', password='12345')
-        neighbourhood = Neighbourhood(hood_name=hood_name, location='Nai', populatio=10, admin=user)
+        neighbourhood = Neighbourhood(hood_name=hood_name, location='Nai', population=10, admin=user)
         
         self.profile = Profile(user=user, bio='me..me', avatar='image.jpg', neighbourhood=neighbourhood)
                 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile, Profile))
+        
+class NameTestCase(TestCase):
+    def setUp(self):
+        self.hood = Name(neighbourhood_name='CBD')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood, Name))
+        
+
         
     
